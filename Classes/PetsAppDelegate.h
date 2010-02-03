b22cecca5eb9045f342fc648c2857af78be0719a
beepscore <support@beepscore.com>
2010-02-03 05:57:56 -0800

    Initial commit.

---
+++ Classes/PetsAppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  PetsAppDelegate.h
+//  Pets
+//
+//  Created by Steve Baker on 2/3/10.
+//  Copyright Beepscore LLC 2010. All rights reserved.
+//
+
+@interface PetsAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
