e26137bb4c81906550ca1c769dc1e405013cc318
beepscore <support@beepscore.com>
2010-02-05 13:22:19 -0800

    Implement header file.

--- Classes/AddViewController.h
+++ Classes/AddViewController.h
@@ -8,9 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+// import base class
+#import "DetailViewController.h"
 
-@interface AddViewController : NSObject {
+// declare protocol.  It could be defined here, but Apple convention
+// defines at end of .h file
+@protocol AddViewControllerDelegate;
 
+@interface AddViewController : DetailViewController {
+    // create a delegate that implements AddViewControllerDelegate protocol
+    id <AddViewControllerDelegate> delegate;
 }
 
+// !!!!:delegate property always uses assign, not retain, to avoid circular retain.
+// Instead, the delegate object will retain the object it is a delegate of,
+// the AddViewController instance.
+@property (nonatomic, assign) id <AddViewControllerDelegate> delegate;
+
+- (void)cancel:(id)sender;
+- (void)save:(id)sender;
 @end
+
+// define protocol required methods
+@protocol AddViewControllerDelegate
+- (void)addViewController:(AddViewController *)controller didFinishWithSave:(BOOL)save;
+@end
+
